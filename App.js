c725ab6ba2c8fc93b91ca425be23cfb5a04364a5
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-02-24 10:58:19 -0300

    Configurando eslint, prettier e editorconfig

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styled.container}>
-        <Text style={styled.welcome}>Hello World</Text>
-      </View>
-    );
-  }
-}
-
-const styled = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -24,3 +14,11 @@ const styled = StyleSheet.create({
     margin: 10,
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello World</Text>
+    </View>
+  );
+}
