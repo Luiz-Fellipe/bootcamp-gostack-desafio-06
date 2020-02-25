e6996a720ede020d41edd8a122818c4356a2d225
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-02-25 10:12:43 -0300

    Adicionando loading

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -32,6 +32,7 @@ export const SubmitButton = styled(RectButton)`
   border-radius: 4px;
   padding: 0 15px;
   margin-left: 10px;
+  opacity: ${props => (props.loading ? 0.7 : 1)};
 `;
 
 export const List = styled.FlatList.attrs({
