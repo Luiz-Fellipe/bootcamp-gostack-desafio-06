e00e56169ef328d4d053b3a8b5f29c2d436c2ad9
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-02-25 08:42:29 -0300

    Estilizando Formulário

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,35 @@
 import styled from 'styled-components/native';
 
+import { RectButton } from 'react-native-gesture-handler';
+
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+  background: #fff;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  border: 1px solid #eee;
+  border-radius: 4px;
   background: #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+  background: #7159c1;
+  border-radius: 4px;
+  padding: 0 15px;
+  margin-left: 10px;
 `;
