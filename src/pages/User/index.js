4da9235eb19ce7c766ad803285d395ebb0cf32f8
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-02-25 11:17:06 -0300

    Buscando informações do usuário na api

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
-
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 // import { Container } from './styles';
 
-export default function User({ route }) {
-  const { user } = route.params;
-  return <View />;
+export default class User extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      setOptions: PropTypes.func,
+    }).isRequired,
+    route: PropTypes.shape({
+      params: PropTypes.shape({
+        user: PropTypes.shape({
+          login: PropTypes.string,
+          name: PropTypes.string,
+        }),
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation, route } = this.props;
+
+    const { user } = route.params;
+
+    navigation.setOptions({ title: user.name });
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    return <View />;
+  }
 }
